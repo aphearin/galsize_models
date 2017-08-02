d3aa69e784b722d0dc30ebd88ee0e87346de57c6
Andrew Hearin <andrew.hearin@yale.edu>
2017-08-02 09:23:45 -0500

    Added conditional_percentile sub-package

---
+++ galsize_models/conditional_percentile/conditional_percentile_functions.py
@@ -0,0 +1,11 @@
+"""
+"""
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+from .engines import conditional_percentile_kernel
+
+
+def calculate_conditional_percentiles(x, y):
+    """
+    """
+    pass
