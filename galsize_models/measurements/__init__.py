51ff86bea1cd14f176e37a117388413f589a0901
Andrew Hearin <andrew.hearin@yale.edu>
2017-08-03 12:55:14 -0500

    Added load_umachine_sdss_with_meert15 function for use with UniverseMachine correlation function calculations

--- galsize_models/measurements/__init__.py
+++ galsize_models/measurements/__init__.py
@@ -1,3 +1,4 @@
 from __future__ import absolute_import, division, print_function, unicode_literals
 
 from .read_meert15 import load_meert15
+from .load_cross_matched_umachine import load_umachine_sdss_with_meert15
---
+++ galsize_models/measurements/load_cross_matched_umachine.py
@@ -0,0 +1,28 @@
+"""
+"""
+import os
+import numpy as np
+from astropy.table import Table
+
+
+__all__ = ('load_umachine_sdss_with_meert15', )
+
+default_datadir = "/Users/aphearin/work/sdss/cross_matched_catalogs/meert15"
+
+
+def load_umachine_sdss_with_meert15(datadir=default_datadir,
+        zmin=-np.inf, zmax=np.inf, sm_low=-np.inf, sm_high=np.inf):
+    """
+    Examples
+    --------
+    >>> full_sdss, is_complete = load_umachine_sdss_with_meert15()
+    """
+    basename = "umachine_sdss_dr10_meert15.hdf5"
+    fname = os.path.join(datadir, basename)
+    full_sdss = Table.read(fname, path='data')
+
+    completeness_table = np.loadtxt(os.path.join(datadir, 'completeness.dat'))
+    completeness_limit = np.interp(full_sdss['sm'], completeness_table[:, 0], completeness_table[:, 1])
+    is_complete = full_sdss['z'] < completeness_limit
+
+    return full_sdss, is_complete
