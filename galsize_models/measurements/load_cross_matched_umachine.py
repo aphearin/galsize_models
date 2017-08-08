0478abd8dcdc12c4d456efa89af09a5813ab1512
Andrew Hearin <andrew.hearin@yale.edu>
2017-08-08 10:17:30 -0500

    Fixed bugs in load_umachine_sdss_with_meert15

--- galsize_models/measurements/load_cross_matched_umachine.py
+++ galsize_models/measurements/load_cross_matched_umachine.py
@@ -10,8 +10,7 @@ __all__ = ('load_umachine_sdss_with_meert15', )
 default_datadir = "/Users/aphearin/work/sdss/cross_matched_catalogs/meert15"
 
 
-def load_umachine_sdss_with_meert15(datadir=default_datadir,
-        zmin=-np.inf, zmax=np.inf, sm_low=-np.inf, sm_high=np.inf):
+def load_umachine_sdss_with_meert15(datadir=default_datadir, zmin=0.02, sm_limit=-np.inf):
     """
     Examples
     --------
@@ -22,8 +21,10 @@ def load_umachine_sdss_with_meert15(datadir=default_datadir,
     full_sdss = Table.read(fname, path='data')
 
     completeness_table = np.loadtxt(os.path.join(datadir, 'completeness.dat'))
-    completeness_limit = np.interp(full_sdss['sm'], completeness_table[:, 0], completeness_table[:, 1])
-    is_complete = full_sdss['z'] < completeness_limit
+
+    assert sm_limit > 9.5, "SDSS volume is too small for completeness thresholds < 9.5"
+    zmax = np.interp(sm_limit, completeness_table[:, 0], completeness_table[:, 1])
+    is_complete = (full_sdss['z'] < zmax) & (full_sdss['z'] >= zmin) & (full_sdss['sm'] > sm_limit)
 
     full_sdss['ssfr'] = np.log10(full_sdss['sfr']/10**full_sdss['sm'])
     return full_sdss, is_complete
