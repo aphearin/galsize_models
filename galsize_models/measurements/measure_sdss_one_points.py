e57c30fdd4a90633243df85168fa9cef5439ffb7
Andrew Hearin <andrew.hearin@yale.edu>
2017-08-08 11:00:04 -0500

    Added tabulate_sdss_size_vs_stellar_mass and load_sdss_size_vs_stellar_mass, and committed new size measurements to repo

--- galsize_models/measurements/measure_sdss_one_points.py
+++ galsize_models/measurements/measure_sdss_one_points.py
@@ -3,8 +3,11 @@
 import os
 import numpy as np
 
+from .load_cross_matched_umachine import load_umachine_sdss_with_meert15
 
-__all__ = ('sdss_size_vs_stellar_mass', )
+
+__all__ = ('sdss_size_vs_stellar_mass', 'tabulate_sdss_size_vs_stellar_mass',
+        'load_sdss_size_vs_stellar_mass')
 
 
 default_datadir = "/Users/aphearin/work/sdss/cross_matched_catalogs/meert15"
@@ -58,7 +61,57 @@ def sdss_size_vs_stellar_mass(logsm, size, redshift, logsm_bins, statistic='mean
     return result, logsm_mids
 
 
-def tabulate_sdss_size_vs_stellar_mass():
+def tabulate_sdss_size_vs_stellar_mass(output_dirname=os.path.abspath('.')):
     """
     """
-    pass
+    logsm_min, logsm_max = 9.75, 11.25
+    full_sdss, __ = load_umachine_sdss_with_meert15(sm_limit=logsm_min)
+
+    dlogsm = 0.15
+    logsm_bins = np.arange(logsm_min, logsm_max+dlogsm, dlogsm)
+
+    mask_all = np.ones(len(full_sdss), dtype=bool)
+    sm = full_sdss['sm'][mask_all]
+    size = full_sdss['r50_magr_kpc_meert15'][mask_all]
+    redshift = full_sdss['z'][mask_all]
+    mean_size_all, logsm_mids = sdss_size_vs_stellar_mass(sm, size, redshift, logsm_bins)
+
+    mask_q = full_sdss['ssfr'] < -11.25
+    sm = full_sdss['sm'][mask_q]
+    size = full_sdss['r50_magr_kpc_meert15'][mask_q]
+    redshift = full_sdss['z'][mask_q]
+    mean_size_q, logsm_mids = sdss_size_vs_stellar_mass(sm, size, redshift, logsm_bins)
+
+    mask_sf = full_sdss['ssfr'] >= -10.75
+    sm = full_sdss['sm'][mask_sf]
+    size = full_sdss['r50_magr_kpc_meert15'][mask_sf]
+    redshift = full_sdss['z'][mask_sf]
+    mean_size_sf, logsm_mids = sdss_size_vs_stellar_mass(sm, size, redshift, logsm_bins)
+
+    mask_gv = (full_sdss['ssfr'] <= -10.75) & (full_sdss['ssfr'] > -11.25)
+    sm = full_sdss['sm'][mask_gv]
+    size = full_sdss['r50_magr_kpc_meert15'][mask_gv]
+    redshift = full_sdss['z'][mask_gv]
+    mean_size_gv, logsm_mids = sdss_size_vs_stellar_mass(sm, size, redshift, logsm_bins)
+
+    np.save(os.path.join(output_dirname, 'logsm_bins'), logsm_bins)
+    np.save(os.path.join(output_dirname, 'mean_size_all'), mean_size_all)
+    np.save(os.path.join(output_dirname, 'mean_size_q'), mean_size_q)
+    np.save(os.path.join(output_dirname, 'mean_size_sf'), mean_size_sf)
+    np.save(os.path.join(output_dirname, 'mean_size_gv'), mean_size_gv)
+
+
+def load_sdss_size_vs_stellar_mass(output_dirname):
+    output_dirname = os.path.abspath(output_dirname)
+    logsm_bins = np.load(os.path.join(output_dirname, 'logsm_bins.npy'))
+    mean_size_all = np.load(os.path.join(output_dirname, 'mean_size_all.npy'))
+    mean_size_q = np.load(os.path.join(output_dirname, 'mean_size_q.npy'))
+    mean_size_sf = np.load(os.path.join(output_dirname, 'mean_size_sf.npy'))
+    mean_size_gv = np.load(os.path.join(output_dirname, 'mean_size_gv.npy'))
+    return (logsm_bins, mean_size_all, mean_size_q, mean_size_sf, mean_size_gv)
+
+
+
+
+
+
