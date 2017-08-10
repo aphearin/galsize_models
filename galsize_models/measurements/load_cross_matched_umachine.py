Fixed bug in completeness calculation of load_umachine_sdss_with_meert15

galsize_models/measurements/load_cross_matched_umachine.py
@@ -23,7 +23,7 @@ def load_umachine_sdss_with_meert15(datadir=default_datadir):
     completeness_table_dirname = "/Users/aphearin/Dropbox/UniverseMachine/data/sdss"
     completeness_table = np.loadtxt(os.path.join(completeness_table_dirname, 'completeness.dat'))
 
-    z_limit = np.interp(np.log10(full_sdss['sm']),
+    z_limit = np.interp(full_sdss['sm'],
             completeness_table[:, 0], completeness_table[:, 1])
     is_complete = (full_sdss['z'] < z_limit)
 