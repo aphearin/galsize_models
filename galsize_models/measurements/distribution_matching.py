06e8aa38a6b7f40c145d864051f356904e7e620b
Andrew Hearin <andrew.hearin@yale.edu>
2017-08-12 20:57:07 -0500

    Added distribution_matching_indices function

---
+++ galsize_models/measurements/distribution_matching.py
@@ -0,0 +1,21 @@
+"""
+"""
+import numpy as np
+
+
+def distribution_matching_indices(distribution1, distribution2, nselect, bins=100):
+    """
+    """
+    hist2, bins = np.histogram(distribution2, density=True, bins=bins)
+    hist1 = np.histogram(distribution1, bins=bins, density=True)[0]
+
+    hist_ratio = np.zeros_like(hist2, dtype=float)
+    hist_ratio[hist2 > 0] = hist1[hist2 > 0]/hist2[hist2 > 0].astype(float)
+
+    bin_mids = 0.5*(bins[:-1] + bins[1:])
+    hist_ratio_interp = np.interp(distribution1, bin_mids, hist_ratio)
+    prob_select = hist_ratio_interp/float(hist_ratio_interp.sum())
+
+    candidate_indices = np.arange(len(distribution1))
+    indices = np.random.choice(candidate_indices, size=nselect, replace=True, p=prob_select)
+    return indices
