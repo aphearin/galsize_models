e8df8084181f99178b7d3eda2773a1456e957159
Andrew Hearin <andrew.hearin@yale.edu>
2017-08-14 09:52:32 -0500

    Fixed bug in distribution_matching_indices

--- galsize_models/measurements/distribution_matching.py
+++ galsize_models/measurements/distribution_matching.py
@@ -7,10 +7,10 @@ def distribution_matching_indices(distribution1, distribution2, nselect, bins=10
     """
     """
     hist2, bins = np.histogram(distribution2, density=True, bins=bins)
-    hist1 = np.histogram(distribution1, bins=bins, density=True)[0]
+    hist1 = np.histogram(distribution1, bins=bins, density=True)[0].astype(float)
 
     hist_ratio = np.zeros_like(hist2, dtype=float)
-    hist_ratio[hist2 > 0] = hist1[hist2 > 0]/hist2[hist2 > 0].astype(float)
+    hist_ratio[hist1 > 0] = hist2[hist1 > 0]/hist1[hist1 > 0]
 
     bin_mids = 0.5*(bins[:-1] + bins[1:])
     hist_ratio_interp = np.interp(distribution1, bin_mids, hist_ratio)
