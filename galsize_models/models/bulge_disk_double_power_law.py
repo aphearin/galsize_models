c8267d5942cd956897aa29f02d69f2aa08020563
Andrew Hearin <andrew.hearin@yale.edu>
2017-08-22 09:31:37 -0500

    Added data_vector_prediction function

--- galsize_models/models/bulge_disk_double_power_law.py
+++ galsize_models/models/bulge_disk_double_power_law.py
@@ -2,19 +2,38 @@
 """
 import numpy as np
 
-__all__ = ('component_size_vs_rhalo', 'galaxy_size_vs_rhalo')
+from ..measurements.measure_one_points import mock_ssfr_sequence_one_points
+from ..measurements.sdss_covariance import assemble_data_vector
 
 
-def component_size_vs_rhalo(rhalo, normalization, alpha, R0=1., scatter=0.):
+__all__ = ('component_size_vs_rhalo', 'galaxy_size_vs_rhalo', 'data_vector_prediction')
+
+
+def component_size_vs_rhalo(rvir_halo_kpc, normalization, alpha, R0=1., scatter=0.):
     """
     """
-    mean_size = normalization*(rhalo/R0)**alpha
+    mean_size = normalization*(rvir_halo_kpc/R0)**alpha
     return 10**np.random.normal(loc=np.log10(mean_size), scale=scatter)
 
 
-def galaxy_size_vs_rhalo(rhalo, bt, norm1, norm2, alpha1, alpha2, R0=1., scatter=0.):
-    size1 = component_size_vs_rhalo(rhalo, norm1, alpha1, R0, scatter)
-    size2 = component_size_vs_rhalo(rhalo, norm2, alpha2, R0, scatter)
+def galaxy_size_vs_rhalo(rvir_halo_kpc, bt, norm1, norm2, alpha1, alpha2, R0=1., scatter=0.):
+    size1 = component_size_vs_rhalo(rvir_halo_kpc, norm1, alpha1, R0, scatter)
+    size2 = component_size_vs_rhalo(rvir_halo_kpc, norm2, alpha2, R0, scatter)
     return bt*size1 + (1-bt)*size2
 
 
+def data_vector_prediction(params, mock, logsm_bins):
+    """
+    """
+    norm1, norm2, alpha1, alpha2, scatter = params
+    rvir_halo_kpc = mock['rvir_halo_kpc']
+    bt = mock['bt_meert15_random']
+    r50_kpc = galaxy_size_vs_rhalo(rvir_halo_kpc, bt, norm1, norm2, alpha1, alpha2,
+                R0=1., scatter=scatter)
+
+    logsm = mock['logsm']
+    mask_sf = mock['is_main_sequence']
+    mask_gv = mock['is_green_valley']
+    mask_q = mock['is_quenched']
+    _x = mock_ssfr_sequence_one_points(logsm_bins, logsm, r50_kpc, mask_sf, mask_gv, mask_q)
+    return assemble_data_vector(*_x)
