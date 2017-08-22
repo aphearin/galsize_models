b8cd46995032cff8a09ec2088a68d854818c046b
Andrew Hearin <andrew.hearin@yale.edu>
2017-08-22 09:08:56 -0500

    Deleted obsolete rvir_models module

--- galsize_models/models/__init__.py
+++ galsize_models/models/__init__.py
@@ -3,7 +3,6 @@
 from __future__ import absolute_import, division, print_function, unicode_literals
 
 
-from .rvir_models import *
 from .load_baseline_catalogs import *
 from .two_point_predictions import *
 from .new_haloprops import *
