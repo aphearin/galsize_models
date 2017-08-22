8289d104afaf6b709bcabed10c3a9abae6586967
Andrew Hearin <andrew.hearin@yale.edu>
2017-08-22 09:11:33 -0500

    Renamed universal_alpha module to bulge_disk_double_power_law

--- galsize_models/models/__init__.py
+++ galsize_models/models/__init__.py
@@ -6,4 +6,4 @@ from __future__ import absolute_import, division, print_function, unicode_litera
 from .load_baseline_catalogs import *
 from .two_point_predictions import *
 from .new_haloprops import *
-from .universal_alpha import *
+from .bulge_disk_double_power_law import *
