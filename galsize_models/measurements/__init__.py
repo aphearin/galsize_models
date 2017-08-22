e4d495e6ce7c3da00a41f3882d64dc48dd047098
Andrew Hearin <andrew.hearin@yale.edu>
2017-08-22 10:53:54 -0500

    Fixed bug in __init__ import

--- galsize_models/measurements/__init__.py
+++ galsize_models/measurements/__init__.py
@@ -2,6 +2,6 @@ from __future__ import absolute_import, division, print_function, unicode_litera
 
 from .read_meert15 import load_meert15
 from .load_cross_matched_umachine import load_umachine_sdss_with_meert15
-from .measure_sdss_one_points import *
+from .measure_one_points import *
 from .rhalf_rvir_power_law_fit import rvir_rhalf_power_law_fit
 from .wp_ratio_mock import wp_ssfr_sequence_mock
