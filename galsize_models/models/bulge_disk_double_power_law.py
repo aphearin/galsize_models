c5a19e6cde27c321ddad2d920f81599d36ceb628
Andrew Hearin <andrew.hearin@yale.edu>
2017-08-25 10:55:47 -0500

    Now using disk_to_bulge_ratio parameter. Adjusted priors accordingly

--- galsize_models/models/bulge_disk_double_power_law.py
+++ galsize_models/models/bulge_disk_double_power_law.py
@@ -26,7 +26,8 @@ def galaxy_size_vs_rhalo(rvir_halo_kpc, bt, norm_bulge, norm_disk, alpha_bulge, 
 def data_vector_prediction(params, mock, logsm_bins, statistic='mean'):
     """
     """
-    norm_bulge, norm_disk, alpha_bulge, alpha_disk, scatter = params
+    norm_bulge, disk_to_bulge_ratio, alpha_bulge, alpha_disk, scatter = params
+    norm_disk = disk_to_bulge_ratio*norm_bulge
     rvir_halo_kpc = mock['rvir_halo_kpc']
     bt = mock['bt_meert15_random']
     r50_kpc = galaxy_size_vs_rhalo(rvir_halo_kpc, bt, norm_bulge, norm_disk, alpha_bulge, alpha_disk,
@@ -44,22 +45,22 @@ def data_vector_prediction(params, mock, logsm_bins, statistic='mean'):
 def lnprior(params):
     """
     """
-    norm_bulge, norm_disk, alpha_bulge, alpha_disk, scatter = params
+    norm_bulge, disk_to_bulge_ratio, alpha_bulge, alpha_disk, scatter = params
 
     acceptable = norm_bulge > 0
-    acceptable *= norm_bulge < 1
+    acceptable *= norm_bulge < 0.1
 
-    acceptable *= norm_disk > 0
-    acceptable *= norm_disk < 1
+    acceptable *= disk_to_bulge_ratio >= 2
+    acceptable *= disk_to_bulge_ratio <= 5
 
-    acceptable *= alpha_bulge > 0
-    acceptable *= alpha_bulge < 2
+    acceptable *= alpha_bulge > 0.5
+    acceptable *= alpha_bulge < 1.5
 
-    acceptable *= alpha_disk > 0
-    acceptable *= alpha_disk < 2
+    acceptable *= alpha_disk > 0.5
+    acceptable *= alpha_disk < 1.5
 
-    acceptable *= scatter > 0
-    acceptable *= scatter < 1
+    acceptable *= scatter > 0.1
+    acceptable *= scatter < 0.4
 
     if bool(acceptable) is True:
         return 0.0
--- scripts/run_double_power_law_mcmc.py
+++ scripts/run_double_power_law_mcmc.py
@@ -20,30 +20,33 @@ from galsize_models.models import mcmc_bulge_disk_double_power_law as mcmc
 ndim = 5
 
 nwalkers = 2*ndim
+mcmc_num_iteration = int(round(args.num_iteration/float(nwalkers)))
+burnin_num_iteration = int(round(args.num_burnin/float(nwalkers)))
+
 p0 = np.random.rand(ndim * nwalkers).reshape((nwalkers, ndim))
 sampler = EnsembleSampler(nwalkers, ndim, mcmc.lnprob, args=mcmc.lnprob_args)
 
 print("...Running burn-in phase of {0} total likelihood evaluations ".format(
-    args.num_burnin*nwalkers))
+    burnin_num_iteration*nwalkers))
 start = time()
-pos0, prob, state = sampler.run_mcmc(p0, args.num_burnin)
+pos0, prob, state = sampler.run_mcmc(p0, burnin_num_iteration)
 sampler.reset()
 end = time()
 print("Total runtime for burn-in = {0:.2f} seconds".format(end-start))
 
-outname = "bulge_disk_power_law_chain.dat"
+outname = "bulge_disk_power_law_chain_aggressive_priors.dat"
 
 
 sep = "  "
 formatter = sep.join("{"+str(i)+":.4f}" for i in range(pos0.shape[-1])) + "  " + "{"+str(pos0.shape[-1])+":.4f}\n"
-header = "norm_bulge,norm_disk,alpha_bulge,alpha_disk,scatter,lnprob\n"
+header = "norm_bulge  disk_to_bulge_ratio  alpha_bulge  alpha_disk  scatter  lnprob\n"
 
 start = time()
 
-print("...Running MCMC with {0} chain elements".format(args.num_iteration*nwalkers))
+print("...Running MCMC with {0} chain elements".format(mcmc_num_iteration*nwalkers))
 with open(outname, "wb") as f:
     f.write(header)
-    for result in sampler.sample(pos0, iterations=args.num_iteration, storechain=False):
+    for result in sampler.sample(pos0, iterations=mcmc_num_iteration, storechain=False):
         pos, prob, state = result
         for a, b in zip(pos, prob):
             newline = formatter.format(*np.append(a, b))
