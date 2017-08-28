0f70b9476954d14a2aa862cac884b94be79dde93
Andrew Hearin <andrew.hearin@yale.edu>
2017-08-28 15:13:09 -0500

    Updated new notebook showing clustering ratios comparing model to data

--- galsize_models/measurements/wp_ratio_mock.py
+++ galsize_models/measurements/wp_ratio_mock.py
@@ -4,37 +4,43 @@ import numpy as np
 from halotools.mock_observables import return_xyz_formatted_array, wp
 
 
-__all__ = ('wp_ssfr_sequence_mock', )
+__all__ = ('wp_size_ratios_mock', )
 
 
-def wp_ssfr_sequence_mock(mock, logsm_min, ssfr_sample, size_key='r50_magr_kpc_meert15',
-        rp_bins=np.logspace(-1, 1.25, 25), pi_max=20., period=250.):
+def wp_size_ratios_mock(mock, logsm_min, sample_cut, size_key='r50_magr_kpc_meert15',
+        rp_bins=np.logspace(-1, 1.25, 20), pi_max=20., period=250.):
     """
     """
     rp_mids = 10**(0.5*(np.log10(rp_bins[:-1]) + np.log10(rp_bins[1:])))
 
     mask_sm_thresh = mock['obs_sm'] > 10**logsm_min
 
-    if ssfr_sample == 'sf':
-        mask_sm_ssfr = mask_sm_thresh & (mock['ssfr'] >= -10.75)
-    elif ssfr_sample == 'q':
-        mask_sm_ssfr = mask_sm_thresh & (mock['ssfr'] < -11.25)
+    if sample_cut == 'sf':
+        sample_mask = mask_sm_thresh & (mock['ssfr'] >= -10.75)
+    elif sample_cut == 'q':
+        sample_mask = mask_sm_thresh & (mock['ssfr'] < -11.25)
+    elif sample_cut == 'bulge':
+        sample_mask = mask_sm_thresh & (mock['bt_meert15_random'] > 0.75)
+    elif sample_cut == 'disk':
+        sample_mask = mask_sm_thresh & (mock['bt_meert15_random'] < 0.25)
+    elif sample_cut == 'mixed':
+        sample_mask = mask_sm_thresh & (mock['bt_meert15_random'] >= 0.25) & (mock['bt_meert15_random'] < 0.75)
     else:
-        raise ValueError("ssfr_sample = {0} not recognized")
+        raise ValueError("sample_cut = {0} not recognized")
 
-    size_cut = np.median(mock[size_key][mask_sm_ssfr])
-    mask_sm_ssfr_small = mask_sm_ssfr & (mock[size_key] < size_cut)
-    mask_sm_ssfr_large = mask_sm_ssfr & (mock[size_key] >= size_cut)
+    size_cut = np.median(mock[size_key][sample_mask])
+    sample_mask_small = sample_mask & (mock[size_key] < size_cut)
+    sample_mask_large = sample_mask & (mock[size_key] >= size_cut)
 
     pos_sm_sf = return_xyz_formatted_array(
             mock['x'], mock['y'], mock['z'], velocity=mock['vz'],
-            velocity_distortion_dimension='z', period=period, mask=mask_sm_ssfr)
+            velocity_distortion_dimension='z', period=period, mask=sample_mask)
     pos_sm_sf_small = return_xyz_formatted_array(
             mock['x'], mock['y'], mock['z'], velocity=mock['vz'],
-            velocity_distortion_dimension='z', period=period, mask=mask_sm_ssfr_small)
+            velocity_distortion_dimension='z', period=period, mask=sample_mask_small)
     pos_sm_sf_large = return_xyz_formatted_array(
             mock['x'], mock['y'], mock['z'], velocity=mock['vz'],
-            velocity_distortion_dimension='z', period=period, mask=mask_sm_ssfr_large)
+            velocity_distortion_dimension='z', period=period, mask=sample_mask_large)
 
     wp_sm = wp(pos_sm_sf, rp_bins, pi_max, period=period)
     wp_sm_small = wp(pos_sm_sf_small, rp_bins, pi_max, period=period)
