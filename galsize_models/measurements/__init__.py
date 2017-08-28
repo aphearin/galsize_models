8ef51bff51b8ffe4cd047a2f98e1020715ee1f29
Andrew Hearin <andrew.hearin@yale.edu>
2017-08-28 12:30:34 -0500

    Updated SDSS clustering measurement modules and notebook

--- galsize_models/measurements/__init__.py
+++ galsize_models/measurements/__init__.py
@@ -1,7 +1,7 @@
 from __future__ import absolute_import, division, print_function, unicode_literals
 
 from .read_meert15 import load_meert15
-from .load_cross_matched_umachine import load_umachine_sdss_with_meert15
+from .load_cross_matched_umachine import *
 from .measure_one_points import *
 from .rhalf_rvir_power_law_fit import rvir_rhalf_power_law_fit
 from .wp_ratio_mock import wp_ssfr_sequence_mock
--- galsize_models/measurements/measure_sdss_clustering.py
+++ galsize_models/measurements/measure_sdss_clustering.py
@@ -14,6 +14,24 @@ _orig_umachine_sdss = Table.read(umachine_sdss_fname, format='ascii.commented_he
 __all__ = ('write_umachine_ascii', 'measure_wp', 'below_median_size_mask')
 
 
+def _copy_auto_regions_data_file(dirname_to_sample_ascii):
+    auto_regions_basename = 'auto_regions.txt'
+    auto_regions_dirname = "/Users/aphearin/Dropbox/UniverseMachine/data/sdss"
+
+    auto_regions_fname1 = os.path.join(dirname_to_sample_ascii, auto_regions_basename)
+    auto_regions_fname2 = os.path.join(auto_regions_dirname, auto_regions_basename)
+
+    if os.path.isfile(auto_regions_fname1):
+        pass
+    else:
+        if os.path.isfile(auto_regions_fname2):
+            command = "cp {0} {1}"
+            os.system(command.format(auto_regions_fname2, auto_regions_fname1))
+        else:
+            raise IOError("The ``auto_regions.txt`` file could not be found in your current directory,\n"
+                "nor in the Dropbox location {1}".format(auto_regions_fname2))
+
+
 def write_umachine_ascii(gals, fname, orig_keys=list(_orig_umachine_sdss.keys()), overwrite=False):
     output_table = Table()
     for key in orig_keys:
@@ -22,6 +40,9 @@ def write_umachine_ascii(gals, fname, orig_keys=list(_orig_umachine_sdss.keys())
 
 
 def get_wp_measurements(wp_sample_fname, sm_low, sm_high, pi_max, num_randoms=int(2e5)):
+
+    _copy_auto_regions_data_file(os.path.dirname(wp_sample_fname))
+
     num_randoms_string = str(int(num_randoms))
 
     code_basename = "/Users/aphearin/work/UniverseMachine/code/UniverseMachine/correl/correl"
@@ -51,20 +72,7 @@ def measure_wp(sample, sm_low, sm_high, dirname, rp_bins=np.logspace(-1, 1.25, 2
             auto_regions_dirname="/Users/aphearin/Dropbox/UniverseMachine/data/sdss"):
     temp_fname = os.path.join(dirname, 'temp.dat')
 
-    auto_regions_basename = 'auto_regions.txt'
-
-    auto_regions_fname1 = os.path.join(dirname, auto_regions_basename)
-    auto_regions_fname2 = os.path.join(auto_regions_dirname, auto_regions_basename)
-
-    if os.path.isfile(auto_regions_fname1):
-        pass
-    else:
-        if os.path.isfile(auto_regions_fname2):
-            command = "cp {0} {1}"
-            os.system(command.format(auto_regions_fname2, auto_regions_fname1))
-        else:
-            raise IOError("The ``auto_regions.txt`` file could not be found in your current directory,\n"
-                "nor in the Dropbox location {1}".format(auto_regions_fname2))
+    _copy_auto_regions_data_file(dirname)
 
     __ = write_umachine_ascii(sample, temp_fname, overwrite=True)
     result = get_wp_measurements(temp_fname, sm_low, sm_high, pi_max)
@@ -78,3 +86,26 @@ def below_median_size_mask(log10sm, rhalf):
     median_log10_size, log10sm_bins, __ = binned_statistic(log10sm, np.log10(rhalf), statistic='median')
     logsm_mids = 0.5*(log10sm_bins[:-1] + log10sm_bins[1:])
     return rhalf < 10**np.interp(log10sm, logsm_mids, median_log10_size)
+
+
+def save_wp_measurement(sample, output_dirname, output_basename, sm_low, sm_high, pi_max=20.):
+    """
+    """
+    assert len(sample) > 900, "Sample size = {0}.\nCan only measure clustering for at least 900 galaxies".format(len(sample))
+
+    msg = "Measuring wp for {0} sample of {1} galaxies"
+    print(msg.format(output_basename, len(sample)))
+
+    rp, wp, _wperr = measure_wp(sample, sm_low, sm_high, output_dirname)
+
+    # temp_ascii_fname = os.path.join(output_dirname, 'tmp.dat')
+    # write_umachine_ascii(sample, temp_ascii_fname, overwrite=True)
+
+    # _results = get_wp_measurements(temp_ascii_fname, sm_low, sm_high, pi_max)
+    # rp, wp, _wperr = _results[:, 0], _results[:, 1], _results[:, 2]
+
+    rp_fname = os.path.join(output_dirname, output_basename+'_rp')
+    wp_fname = os.path.join(output_dirname, output_basename+'_wp')
+    np.save(wp_fname, wp)
+    np.save(rp_fname, rp)
+
