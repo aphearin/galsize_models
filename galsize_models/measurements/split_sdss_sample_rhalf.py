3ed0cc8c26cd5ad0d1e2f10dfe1c17f50095ce79
Andrew Hearin <andrew.hearin@yale.edu>
2017-08-29 10:50:34 -0500

    Added figure showing lack of impact on lensing

--- galsize_models/measurements/split_sdss_sample_rhalf.py
+++ galsize_models/measurements/split_sdss_sample_rhalf.py
@@ -1,10 +1,10 @@
 """
 """
-from numpy import interp
+import numpy as np
 from scipy.stats import binned_statistic
 
 
-__all__ = ('sdss_sample_below_median_size', )
+__all__ = ('sdss_sample_below_median_size', 'mock_sample_below_median_size')
 
 
 def sdss_sample_below_median_size(sample):
@@ -14,5 +14,17 @@ def sdss_sample_below_median_size(sample):
     r50 = sample['r50_magr_kpc_meert15']
     median_size_table, logsm_bin_edges, __ = binned_statistic(logsm, r50, statistic='median')
     logsm_table = 0.5*(logsm_bin_edges[:-1] + logsm_bin_edges[1:])
-    size_cut = interp(logsm, logsm_table, median_size_table)
+    size_cut = np.interp(logsm, logsm_table, median_size_table)
     return r50 < size_cut
+
+
+def mock_sample_below_median_size(sample, size_key='r50_magr_kpc_meert15'):
+    """
+    """
+    logsm = np.log10(sample['obs_sm'])
+    r50 = sample[size_key]
+    median_size_table, logsm_bin_edges, __ = binned_statistic(logsm, r50, statistic='median')
+    logsm_table = 0.5*(logsm_bin_edges[:-1] + logsm_bin_edges[1:])
+    size_cut = np.interp(logsm, logsm_table, median_size_table)
+    return r50 < size_cut
+
